7fd7df4d64e032947f929bad1aebbfdc1f494a26
lucknovice <243284589@qq.com>
2015-08-27 15:18:22 +0800

    将jdbc转化成用Mybatis，并增加log4j日志

--- src/com/imooc/dao/MessageDao.java
+++ src/com/imooc/dao/MessageDao.java
@@ -1,20 +1,46 @@
 package com.imooc.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.imooc.bean.Message;
+import org.apache.ibatis.session.SqlSession;
 
+import com.imooc.bean.Message;
+import com.imooc.db.DBAccess;
+
+/*
+ * 
+ */
+public class MessageDao {
+	/*
+	 * 根据查询条件查询数据列表
+	 */
+	public List<Message> queryMessageList(String command,String description){
+		DBAccess dbAccess = new DBAccess();
+		List<Message> messageList = new ArrayList<Message>();
+		SqlSession sqlSession = null;
+		try {
+		sqlSession = dbAccess.getSqlSession();
+		Message message = new Message();
+		message.setCommand(command);
+		message.setDescription(description);
+		messageList = sqlSession.selectList("Message.queryMessageList",message);
+		//通过Sqlsession执行sql语句
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			if(sqlSession != null){
+				sqlSession.close();
+			}
+		}
+		return messageList;
+	}
 /*
  * 根据查询条件查询数据列表
  */
-public class MessageDao {
-	public List<Message> queryMessageList(String command,String description) {
+	/*public List<Message> queryMessageList(String command,String description) {
 		List<Message> list = new ArrayList<Message>();
 		Connection connection = null;
 		try {
@@ -53,5 +79,5 @@ public class MessageDao {
 		}
 		return list;
 		
-	}
+	}*/
 }
